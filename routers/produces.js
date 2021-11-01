7ec7e838166f9efa70b29aaf647edfd194a32b88
mferraris <mgl.ferraris@gmail.com>
2021-11-01 23:07:54 +0100

    set up 'get produces by country' endpoint

--- routers/produces.js
+++ routers/produces.js
@@ -2,10 +2,28 @@ const { Router } = require("express");
 const router = new Router();
 
 const Produce = require("../models").produce;
+const Country = require("../models").country;
 
+// get all produces
 router.get("/produce/all", async (req, res, next) => {
   try {
     const produces = await Produce.findAll();
+
+    res.send(produces);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get all produces per country
+router.get("/:countryId/produce/all", async (req, res, next) => {
+  try {
+    const countryId = req.params.countryId;
+
+    const produces = await Country.findOne({
+      where: { id: countryId },
+      include: [Produce],
+    });
 
     res.send(produces);
   } catch (error) {
