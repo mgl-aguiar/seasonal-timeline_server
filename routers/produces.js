7497bac34ff79bd1c0f36c5209e66ac0fff48e6d
mferraris <mgl.ferraris@gmail.com>
2021-11-03 13:36:21 +0100

    set up "get produce detaiils" endpoint

--- routers/produces.js
+++ routers/produces.js
@@ -16,7 +16,7 @@ router.get("/produce/all", async (req, res, next) => {
 });
 
 //get all produces per country
-router.get("/produce/:countryId", async (req, res, next) => {
+router.get("/produce/country/:countryId", async (req, res, next) => {
   try {
     const countryId = req.params.countryId;
 
@@ -26,6 +26,23 @@ router.get("/produce/:countryId", async (req, res, next) => {
     });
 
     res.send(produces);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get produce details
+router.get("/produce/details/:produceId", async (req, res, next) => {
+  try {
+    const produceId = parseInt(req.params.produceId);
+
+    const produceDetails = await Produce.findByPk(produceId);
+
+    if (produceDetails) {
+      res.send(produceDetails);
+    } else {
+      res.status(404).send({ message: "Produce not found" });
+    }
   } catch (error) {
     next(error);
   }
